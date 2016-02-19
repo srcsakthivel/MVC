735f76852b57a725e87266ac22969dacb696183b
sakthivel <srcsakthivel@yahoo.co.in>
2016-02-19 00:26:08 -0500

    Simple Home Controller, Index Action and Index View

--- MVC.GetStarted/MVC.GetStarted/App_Start/RouteConfig.cs
+++ MVC.GetStarted/MVC.GetStarted/App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace MVC.GetStarted
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "Default",                                                                    //Name
+                url: "{controller}/{action}/{id}",                                                  //URL Parameter
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional } //Defaults
             );
         }
     }
